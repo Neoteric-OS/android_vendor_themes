be721c42b801a8bd2b589cbe149e81a843c954dd
HELLBOY017 <abhaygill017@gmail.com>
2023-03-14 15:34:20 +0530

    themes: Add fonts from sc-v2 branch

---
+++ fonts.mk
@@ -0,0 +1,42 @@
+LOCAL_PATH := vendor/themes
+
+# Fonts
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/themes/fonts//prebuilt,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+
+PRODUCT_PACKAGES += \
+    FontInterOverlay \
+    FontLinotteSourceOverlay \
+    FontManropeOverlay \
+    FontOnePlusSansOverlay \
+    FontOneplusSlateSourceOverlay \
+    FontAppleMintOverlay \
+    FontCartoonistHandOverlay \
+    FontChinacatOverlay \
+    FontLatoOverlay \
+    FontRosemaryOverlay \
+    FontSanFransiscoOverlay \
+    FontSlateProOverlay \
+    FontZillaSlabOverlay \
+    FontCoreMellow \
+    FontFucek \
+    FontLemon \
+    FontSamsungSans \
+    FontSimpleDay \
+    FontUbuntu \
+    FontBariolSourceOverlay \
+    FontAclonicaSourceOverlay \
+    FontAmaranteSourceOverlay \
+    FontCagliostroSourceOverlay \
+    FontComfortaaSourceOverlay \
+    FontComicSansSourceOverlay \
+    FontCoolstorySourceOverlay \
+    FontLGSmartGothicSourceOverlay \
+    FontSamsungOneSourceOverlay \
+    FontSonySketchSourceOverlay \
+    FontSurferSourceOverlay \
+    FontStoropiaOverlay \
+    FontNothiingDotOverlay \
+    FontOppoSansOverlay \
+    FontProductSansVHOverlay \
+    FontNothingDotHeadlineOverlay
