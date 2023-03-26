2f0c3236a42111410c3d5e992f338f997baf26c0
Dominik Procházka <dominikprochazka100@gmail.com>
2023-03-26 20:19:33 +0200

    themes: Move Icon packs & shapes to themes

--- common.mk
+++ common.mk
@@ -1,6 +1,12 @@
 # Fonts
 include vendor/themes/fonts.mk
 
+# Icon Packs
+include vendor/themes/iconpacks/iconpacks.mk
+
+# Icon Shapes
+include vendor/themes/iconshapes/iconshapes.mk
+
 PRODUCT_PACKAGES += \
     ZephyrusThemesStub \
     BlackTheme
