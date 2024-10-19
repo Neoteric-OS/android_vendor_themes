b1290041229d413f08deffed976ad0396bc3f07d
Abhay Singh Gill <abhaygill017@gmail.com>
2024-10-20 01:21:03 +0530

    themes: Initial config for vic
    Based on our tm branch
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

---
+++ config.mk
@@ -0,0 +1,12 @@
+# Fonts
+include vendor/themes/fonts/fonts.mk
+
+# Icon Packs
+include vendor/themes/iconpacks/iconpacks.mk
+
+# Icon Shapes
+include vendor/themes/iconshapes/iconshapes.mk
+
+# UI packages
+PRODUCT_PACKAGES += \
+    BlackTheme
